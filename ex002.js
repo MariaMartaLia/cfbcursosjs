fee0a7426b798bfa045caf1284effeef2e3b14d4
Maria Marta (Lia) <160326282+MariaMartaLia@users.noreply.github.com>
2024-03-11 09:42:01 -0300

    ex002 modo strict

---
+++ ex002.js
@@ -0,0 +1,14 @@
+"use strict"
+nome = 'bruno'
+console.log('bruno')
+console.log(nome)
+console.log(`O nome é ${nome}`)
+
+
+
+
+//Mode sem strict//
+/*nome = 'bruno'
+console.log('bruno')
+console.log(nome)
+console.log(`O nome é ${nome}`)*/
