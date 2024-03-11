d42ae5594980b0d84933a74ef62c6aa404491e93
Maria Marta (Lia) <160326282+MariaMartaLia@users.noreply.github.com>
2024-03-11 10:11:01 -0300

    ex004 operadores aritmeticos

---
+++ ex004.js
@@ -0,0 +1,13 @@
+//let n1 =0, n2 =0 //
+/*let num1=0
+let num2=0*/
+/*let n1,n2,n3
+n1=n2=n3=0
+console.log(n1)
+console.log(n2)
+console.log(n3)*/
+let num1=0, num2=0, res=0
+num1=5
+num2=10
+res=num1+num2
+console.log(res)
