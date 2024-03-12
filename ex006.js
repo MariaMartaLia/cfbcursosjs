c74a0b1c8ae0e9ee839e2bb449328bb003074168
Maria Marta (Lia) <160326282+MariaMartaLia@users.noreply.github.com>
2024-03-12 08:54:24 -0300

    loop for

---
+++ ex006.js
@@ -0,0 +1,12 @@
+//Loop For//
+
+/*for(let i = 0; i < 100;i++){
+    console.log(`Seus projetos são ${i}`)
+}*/
+ for( let i = 0; i < 100;i++){
+    if(i%2==0){
+        console.log(`${i} é Par!`)
+    }else{
+        console.log(`${i} é Impar!`)
+    }
+ }
