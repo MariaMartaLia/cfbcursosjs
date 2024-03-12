bc55eeea8fcc6794df3944f2a5e4fec7d7d29223
Maria Marta (Lia) <160326282+MariaMartaLia@users.noreply.github.com>
2024-03-12 09:39:49 -0300

    do while

---
+++ ex009.js
@@ -0,0 +1,13 @@
+let n = 10
+do{console.log(`numero menor que 10`)
+n++
+}while(n<10)
+console.log(`Fim do programa!!`)
+
+
+
+/*while(n<10){
+    console.log(`numero menor que 10`)
+    n++
+}
+console.log(`Fim do programa!!`)*/
