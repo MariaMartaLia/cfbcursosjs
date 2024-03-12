305f73cc82db974818376043379fdae543ae63d8
Maria Marta (Lia) <160326282+MariaMartaLia@users.noreply.github.com>
2024-03-12 08:34:24 -0300

    if else e switch case

---
+++ ex005.js
@@ -0,0 +1,24 @@
+/*let n1= 5
+let n2=1
+if(n1>10){
+    console.log('numero maior que 10')
+}else{
+    console.log('numero menor que 10')
+}
+console.log('fim do programa')*/
+/*
+ let colocacao = 6
+
+ switch(colocacao){
+    case 1:
+    console.log('Primeiro Lugar!')
+    break
+    case 2:
+        console.log('Segundo Lugar')
+    break
+    case 3:
+        console.log('Terceiro lugar')
+        break
+        default:
+            console.log(' Nao subiu ao podio')
+ }*/
