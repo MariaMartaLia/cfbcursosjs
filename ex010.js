867d7c2e2d956f07403b9e18d232bb3e43a813ce
Maria Marta (Lia) <160326282+MariaMartaLia@users.noreply.github.com>
2024-03-12 09:59:33 -0300

    break e continue

---
+++ ex010.js
@@ -0,0 +1,28 @@
+let n = 0
+let max = 1000
+let par=0
+    for(let i = n; i<max;i++){
+        
+        if(i%2!=0){
+            continue
+        }
+        par++
+    }
+    console.log(`A quantidade ne numeros pares são${par}`)
+    console.log(`Fim do programa!`)
+
+
+
+
+
+
+ /*while( n <  max){
+    console.log(`Número ${n} menor que 1000`) 
+    if(n >=10){
+        break
+    }
+        n++
+    }
+    console.log(`Fim do programa!`)*/
+
+ 
---
+++ node_e39ddbff9b114.tmp.js
@@ -0,0 +1,22 @@
+let n = 0
+let max = 1000
+
+
+while( n <  max){
+    console.log(`Número ${n} menor que 1000`) 
+    if(n >=10){
+        continue
+    }
+        n++
+    }
+    console.log(`Fim do programa!`)
+ /*while( n <  max){
+    console.log(`Número ${n} menor que 1000`) 
+    if(n >=10){
+        break
+    }
+        n++
+    }
+    console.log(`Fim do programa!`)*/
+
+ 
