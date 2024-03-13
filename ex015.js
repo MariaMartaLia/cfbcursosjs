91a0f2164273cbd1e8d2daeca006b9d459c4ff7a
Maria Marta (Lia) <160326282+MariaMartaLia@users.noreply.github.com>
2024-03-13 09:39:56 -0300

    funcoes anonima

---
+++ ex015.js
@@ -0,0 +1,23 @@
+const f = new Function('v1','v2',"return v1+v2")// Função construtor anonima
+console.log(f(10,20))
+
+
+
+
+
+/*const f = function(...valores){
+    let res = 0
+    for(v of valores){
+        res+=v
+    }
+    return res
+}
+console.log(f(10,20))*/
+
+
+
+
+/*let f = function(v1,v2){
+    return v1+v2
+}
+console.log(f(10,20))*/
